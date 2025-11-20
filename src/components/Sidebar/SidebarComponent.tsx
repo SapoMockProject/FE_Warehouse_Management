68df813fa09c72fb040422c4959e906011b56189
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2025-11-20 12:00:20 +0700

    fix svg

--- src/components/Sidebar/SidebarComponent.tsx
+++ src/components/Sidebar/SidebarComponent.tsx
@@ -32,7 +32,6 @@ export const SidebarComponent = () => {
                         viewBox="-4.5 0 20 20"
                         version="1.1"
                         xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink"
                         fill="#000000"
                     >
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
